84771ef2961ca0f60151e0d065fa6e1af69d02c9
Benis Tambe <will.jenifer@yahoo.com>
2021-03-26 19:45:26 -0400

    commit tree a0900b0a90d51f3dadcfa40a1f490ed005ea679c

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,32 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "Write your SQL query Here"
+  # "SELECT projects.title, SUM(pledges.amount) from pledges INNER JOIN projects on pledges.project_id = projects.id ORDER BY(title)"
+  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
+
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Write your SQL query Here"
+  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges on users.id = pledges.user_id GROUP BY users.name ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Write your SQL query Here"
+  # "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM projects JOIN pledges ON projects.id = pledges.id GROUP BY projects.title HAVING (SUM(pledges.amount) - projects.funding_goal) >= 0"
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left >= 0;"
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  # "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id ORDER BY amount"
+  "SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount);"
+
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  # "SELECT projects.category, SUM(pledges.amount) from projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.category  WHERE projects.category = 'music' " #Why not HAVING? instead of WHERE
+  "SELECT projects.category, pledges.amount from projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'music' " #Why not HAVING? instead of WHERE
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT projects.category, SUM(pledges.amount) from projects JOIN pledges on projects.id = pledges.project_id WHERE projects.category = 'books'"
 end
